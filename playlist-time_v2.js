7c35f30a5bc7068fb2286282d6d5f4390bb48d52
ThalysSilva <Thalysfarias11@gmail.com>
2020-07-05 23:59:27 -0300

    bugfix
    corrigido algumas playlists que não funcionavam.

--- playlist-time_v2.js
+++ playlist-time_v2.js
@@ -1,21 +1,26 @@
 (function() {
-var playlist = document.querySelector("#contents"),
-    timeSeconds = 0,
-    timesContainer = playlist.querySelectorAll(".style-scope ytd-thumbnail-overlay-time-status-renderer");
+    var playlist = document.querySelectorAll("#contents"),
+        timeSeconds = 0,
+        timesContainer = null;
 
-for(var i = 0; i < timesContainer.length; i++){
-    var timeStr = timesContainer[i].innerText,
-	timeParts = timeStr.split(":"),
-	seconds = (timeParts[0] * 60) + parseInt(timeParts[1]);
+    for(var i = 0; i < playlist.length; i++){
+
+        timesContainer = playlist[i].querySelectorAll(".style-scope ytd-thumbnail-overlay-time-status-renderer");
+
+    }
+    for(var i = 0; i < timesContainer.length; i++){
+        var timeStr = timesContainer[i].innerText,
+        timeParts = timeStr.split(":"),
+        seconds = (timeParts[0] * 60) + parseInt(timeParts[1]);
+        
+        timeSeconds += seconds;
+    }
     
-    timeSeconds += seconds;
-}
-
-var hours = (timeSeconds / 60) / 60,
-    minutes = (timeSeconds / 60) % 60,
-    seconds = (timeSeconds % 60),
-    result = parseInt(hours) + ":" + parseInt(minutes) + ":" + parseInt(seconds);
-
-alert(result);
-
-})();
+    var hours = (timeSeconds / 60) / 60,
+        minutes = (timeSeconds / 60) % 60,
+        seconds = (timeSeconds % 60),
+        result = parseInt(hours) + ":" + parseInt(minutes) + ":" + parseInt(seconds);
+    
+    alert(result);
+    
+    })();
